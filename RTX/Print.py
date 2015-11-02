0b2e3e71e5aaf1d6c0e831ab40798890ff3b28ae
llfccc <llfccc@live.cn>
2015-11-02 10:50:19 +0800

    Update Print.py

--- RTX/Print.py
+++ RTX/Print.py
@@ -2,23 +2,14 @@
 
 import xlwt
 
-#a=[1,2,3]
-#b=[4,5,6]
-#c=[[a,b] for a,b in zip(a,b)]
-#print c
-
-
-
 d=[['1','a','XXX'],['2','b','ooo'],['3','c','xoo'],['4','d','oox']]
-
-
+##---将结果写入excel文件，保存为results.xls的sheet1表   ---##
 w = xlwt.Workbook()     #创建一个工作簿
 ws = w.add_sheet('Sheet1')     #创建一个工作表
-for i in range(0,len(d),1):
+for i in range(0,len(d),1):     #外循环，d[i]代表每一条完整的记录，包括主题、参与者、对话内容等
     print i
-    for j in range(0,len(d[0]),1):
+    for j in range(0,len(d[0]),1):  #内循环，d[i][j]代表每一条完整记录中某一列的内容
         print j
-        ws.write(i,j,d[i][j])    #在1行1列写入bit
-       
-        
-w.save('mini.xls')     #保存
+        ws.write(i,j,d[i][j])    #在i行j列写入d[i][j]
+ 
+w.save('results.xls')     #保存
